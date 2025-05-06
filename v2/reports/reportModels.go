2cd3dc0d06fea2c94c917b9cf3f2a65f50e659c0
Anton Erne <antonerne@yahoo.com>
2025-05-07 06:58:09 +0900

    May07-0658

--- v2/reports/reportModels.go
+++ v2/reports/reportModels.go
@@ -1,7 +1,6 @@
 package reports
 
 import (
-	"fmt"
 	"strings"
 	"time"
 
@@ -245,7 +244,6 @@ func (mt *MissionType) GetScheduledTime(sens []string,
 
 func (mt *MissionType) GetExecutedTime(sens []string,
 	gs *systemdata.GroundSystem, enclave string) uint {
-	fmt.Println(gs.ID)
 	answer := uint(0)
 	for _, msn := range mt.Missions {
 		senMax := uint(0)
